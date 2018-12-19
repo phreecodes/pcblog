29d0529c6376e23bc74102071e87e41fd87d515a
Etieneabasi Sunday Asuquo <abasi.asuquo@gmail.com>
2018-12-19 14:13:35 +0100

    Etieneabasi

---
+++ post_work/index.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="bootstrap/fonts/css/all.css">
+<section class=" loginSection d-flex justify-content-center align-items-center">
+    <div class="divLogin ">
+        <div class="topLogin">
+            
+        </div>
+        <form action="" method="POST">
+            <div class="divContainer">
+        <label for="inputEmail3" class=" col-form-label">Username</label>
+  <div class="form-group row">
+   
+    <div class="col-md-12">
+      <input type="text" name="username" class="form-control inputEmail" id="inputEmail3" placeholder="Username">
+    </div>
+  </div>
+  <label for="inputPassword3" class=" col-form-label">Password</label>
+  <div class="form-group row">
+    <div class="col-md-12">
+      <input type="password" name ="password"class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="col-md-12 forgotText">
+     <span class="text-danger lead ">Forgot password ?</span> 
+    </div>
+  </div>
+  <fieldset class="form-group">
+   
+      <div class="form-group row">
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+</div>
+</form>
+    
+    </div>
+</section>
