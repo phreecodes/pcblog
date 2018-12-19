1fac6bd311b6f4ae309b53778ebc49f7f51268bf
Evidence Etinih Ekanem <43958700+EvidenceEkanem@users.noreply.github.com>
2018-12-19 14:42:05 +0100

    added header and footer

---
+++ footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+</head>
+<body>
+<style>
+.btn-dark{
+    border: 2px solid white;
+}
+</style>
+<section class="bg-dark">
+    <footer class="container">
+
+        <div class="d-flex justify-content-center align-item-center">
+            <img src="images/HP_logo.png" alt="logo" height="50px">
+            <a class="navbar-brand ml-2 font-weight-bold text-white" href="#">BLOG</a>
+        </div>
+
+        <div>
+            <p class="d-flex justify-content-center align-item-center text-white">For more information you can contact the Admin</p>
+            <div class="d-flex justify-content-center align-item-center">
+                <button class="mr-2  btn-dark"><a href="https://www.facebook.com/evidence.ekanem.75" class="fab fa-facebook text-white"><span class="mx-2 text-white">Facebook<span></a></button>
+                <button class="mr-2 btn-dark"><a href="https://twitter.com/EkanemEvidence?=09" class="fab fa-twitter text-white"><span class="mx-2 text-white">Twitter<span></a></button>
+                <button class="mr-2  btn-dark"><a href="https://www.linkedin.com/in/evidence-ekanem-125b37155" class="fab fa-linkedin text-white"><span class="mx-2 text-white">LinkedIn<span></a></button>
+                <button class="btn-dark"><a href="https://www.instagram.com/p/Bm1mJHQFHzy/?utm_source=ig_share_sheet&igshid=neexu9q3zqb2" class="fab fa-instagram text-white"><span class="mx-2 text-white">Instagram<span></a></button>                                  
+            </div>                      
+        </div>
+        
+    </footer>
+    <div class="bg-secondary d-flex justify-content-center align-item-center py-1 mt-2">
+        <i class="fas fa-copyright mt-1 mr-1"></i>
+        <span class="text-light">2018. All right reserved | Designed by Project Create</span>
+    </div>
+</section>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>header</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+</head>
+<body>
+    <section class="container"><!-- HEADER-->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            
+            <img src="images/HP_logo.png" alt="logo" height="50px">
+            <!-- <i class="fa fa-btc" height=""></i> -->
+            <a class="navbar-brand ml-2 font-weight-bold" href="#">BLOG</a>
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#"><input type="submit" class="btn btn-primary" value="Log In"></a>
+                </li>
+            </ul>
+            <!-- <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-primary my-2 my-sm-0" type="submit">Log in</button>
+            </form> -->
+        </div>
+    </nav>
+    </section>
+</body>
+</html>
